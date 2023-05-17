53cef8b6224178bac44392f4214add1331bd6d80
nareman995 <nareman.kandeel@hotmail.com>
2023-05-17 12:12:44 +0300

    edit radio button check in js

--- Project JS/JS/form_registerjavascript.js
+++ Project JS/JS/form_registerjavascript.js
@@ -4,6 +4,7 @@ let user={}
 let users=[]
 Submit_btn.addEventListener("click",()=>
 {
+
    save_user()
   
 })
@@ -12,8 +13,18 @@ function save_user()
     let saved_email=window.localStorage.getItem(all_items[8].value);
     if(saved_email===null&&all_items[8].value!=="")
     {
-all_items.forEach(item=>{
-    user[item.id]=item.value});
+          all_items.forEach(item=>{
+                user[item.id]=item.value});
+    if(all_items[7].checked)
+    { 
+      delete user["male"];  
+
+
+    }
+    else if(all_items[6].checked)
+    {
+         delete user["female"]
+    }
 delete user["submit_btn"];
 delete user["reset_btn"];
 window.localStorage.setItem(all_items[8].value,JSON.stringify(user));
