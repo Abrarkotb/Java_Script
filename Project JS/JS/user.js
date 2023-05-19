31da323906526ffa86304bc2257d9773e4f7bb1d
abrar kotb <abrarkotb4@gmail.com>
2023-05-19 23:25:07 +0300

    update password js

--- Project JS/JS/user.js
+++ Project JS/JS/user.js
@@ -1,9 +1,48 @@
 
 function myFunction() {
-    var x = document.getElementById("myInput");
+    var x = document.getElementById("pass_old");
     if (x.type === "password") {
       x.type = "text";
     } else {
       x.type = "password";
     }
   }
+  function myFunction_2() {
+    var x = document.getElementById("pass_new");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+
+if( localStorage.setItem('password') ===  document.getElementById('pass_old').value)  
+  function changePassBtnClick(){
+    localStorage.setItem('password', document.getElementById('pass_new').value);
+    alert('Password changed');
+  }
+
+  function loginBtnClick(){
+    if(document.getElementById('setnew_pass').value == localStorage.getItem('password')){
+      alert('Correct Login');
+    }else{
+      alert('Wrong Password');
+    }
+  }
+
+
+
+
+
+//   if(!localStorage.getItem('password')){
+//     localStorage.setItem('password', 'pass');
+//   }
+
+
+//   function loginBtnClick(){
+//     if(document.getElementById('login').value == localStorage.getItem('password')){
+//       alert('Correct Login');
+//     }else{
+//       alert('Wrong Password');
+//     }
+//   }
