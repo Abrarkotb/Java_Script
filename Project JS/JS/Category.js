309c1b26663212254eb8d03253aead7cfe70ca69
nareman995 <nareman.kandeel@hotmail.com>
2023-05-21 05:25:51 +0300

    everything okay

--- Project JS/JS/Category.js
+++ Project JS/JS/Category.js
@@ -1,8 +1,39 @@
-// let contect = document.querySelectorAll('#apis');
-// let out ="";
-// fetch("https://api.thecatapi.com/v1/images/search?limit=10")
-// .then((response)=> response.json())
-// .then((x-api-key) => {
-//     return 
+let content = document.querySelector("#content");
+
+let out = "";
+fetch("https://www.googleapis.com/books/v1/volumes?q=quilting")
+.then((response) => response.json())
+.then((books) => {
+  // console.log(books)
+    return books.items.map(book => {      
+      out += `
+
+      <div class="card col-3">
+        <div class="card-header" style="height: 60px;">
+          <h5 class="card-title limit-paragraph-line">${book.volumeInfo.title}</h5>
+        </div>
+        <img class="card-img-top" height="400"  src="${book.volumeInfo.imageLinks.smallThumbnail}" alt="${book.volumeInfo.subtitle}">
+        <div class="card-body">
+          <p class="card-text limit-paragraph-line">${book.volumeInfo.description}</p>
+        </div>
+        <div class="card-footer bg-secondary text-white">
+          <small class="text-muted "> 
+            <button class="btn add-to-plan" id="${book.id}" onClick="singleBook(this.id)" > read More</button>
+          </small>
+        </div>
+      </div>
+    
      
-// })
+
+    `;
+      content.innerHTML = out;
+    })
+});
+
+
+
+function singleBook(id){
+  
+    window.location.href = "single-page.html?" + id;
+
+}
